331066a636da7b0ef5c0801111dc7b5a08886410
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-07-22 08:36:26 +0200

    fix perf timeout

---
+++ perf/helpers/eventually.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+
+class eventually():
+
+  def __init__(self, timeout=2):
+    self.__inblock = False
+    self.__last_exception = None
+    self.__timeout = timeout
+    self.__block = lambda *args: None
+
+  def __get__(self, instance, *args):
+    return partial(self.__call__, instance)
+
+  def __call__(self, *args, **kwargs):
+    if not self.__inblock:
+      self.__block = args[0]
+      self.__inblock = True
+      return self
+
+    deadline = time.monotonic() + self.__timeout
+    while deadline > time.monotonic():
+      try:
+        return self.__block(*args, **kwargs)
+      except (AssertionError, Exception) as ex:
+        self.__last_exception = ex
+        time.sleep(1)
+
+    if self.__last_exception:
+      raise self.__last_exception
---
+++ perf/helpers/shell.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import subprocess
+import threading
+import signal
+import time
+import os
+import gc
+from utils import print_daemon
+
+
+class Deadline(threading.Thread):
+
+  def __init__(self, timeout, callback):
+    super().__init__(daemon=True)
+    self.__timeout = timeout
+    self.__callback = callback
+    self.__cancelled = threading.Event()
+
+  def run(self) -> None:
+    deadline = time.monotonic() + self.__timeout
+    while not self.__cancelled.wait(deadline - time.monotonic()):
+      if not self.__cancelled.is_set() and deadline <= time.monotonic():
+        return self.__callback()
+
+  def cancel(self) -> None:
+    self.__cancelled.set()
+    self.join()
+
+
+def execute(command, timeout=20) -> None:
+  if not silent:
+    print_daemon(' '.join(command))
+
+  try:
+    p = subprocess.Popen(
+      command,
+      shell=False,
+      stdin=None,
+      stdout=subprocess.PIPE,
+      stderr=subprocess.PIPE,
+      close_fds=True
+    )
+
+    def kill() -> None:
+      for sig in [signal.SIGTERM, signal.SIGQUIT, signal.SIGKILL, signal.SIGKILL]:
+        if p.poll():
+          break
+        try:
+          os.kill(p.pid, sig)
+        except OSError:
+          break
+
+    deadline = Deadline(timeout, callback=kill)
+    deadline.start()
+    (result, error) = p.communicate()
+    deadline.cancel()
+
+    result = result.decode('utf-8').strip() if result else ''
+    error = error.decode('utf-8').strip() if error else ''
+    code = p.returncode
+
+    del p
+
+    gc.collect()
+
+    return (code, result, error)
+  except subprocess.CalledProcessError:
+    return (-1, None, None)
