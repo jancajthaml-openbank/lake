c65c9acd09541349bdfc3c9a7370284a51b62836
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-07-24 22:00:13 +0200

    simplify black box tests

--- bbtest/helpers/unit.py
+++ bbtest/helpers/unit.py
@@ -31,15 +31,16 @@ class UnitHelper(object):
       'armv8': 'arm64'
     }.get(platform.uname().machine, 'amd64')
 
-  def __init__(self):
+  def __init__(self, context):
     self.arch = self.get_arch()
 
-    self.store = {}
+    self.store = dict()
     self.image_version = None
     self.debian_version = None
-    self.units = {}
-    self.services = []
+    self.units = dict()
+    self.services = list()
     self.docker = docker.APIClient(base_url='unix://var/run/docker.sock')
+    self.context = context
 
   def download(self):
     try:
@@ -55,23 +56,26 @@ class UnitHelper(object):
     if self.debian_version.startswith('v'):
       self.debian_version = self.debian_version[1:]
 
-    scratch_docker_cmd = ['FROM alpine']
-
     image = 'openbank/lake:{}'.format(self.image_version)
-    package = 'lake_{}_{}'.format(self.debian_version, self.arch)
-    scratch_docker_cmd.append('COPY --from={} /opt/artifacts/{}.deb /tmp/packages/lake.deb'.format(image, package))
+    package = '/opt/artifacts/lake_{}_{}.deb'.format(self.debian_version, self.arch)
+    target = '/tmp/packages/lake.deb'
 
     temp = tempfile.NamedTemporaryFile(delete=True)
     try:
-      with open(temp.name, 'w') as f:
-        for item in scratch_docker_cmd:
-          f.write("%s\n" % item)
+      with open(temp.name, 'w') as fd:
+        fd.write(str(os.linesep).join([
+          'FROM alpine',
+          'COPY --from={} {} {}'.format(image, package, target)
+        ]))
 
-      for chunk in self.docker.build(fileobj=temp, rm=True, decode=True, tag='bbtest_artifacts-scratch'):
-        if 'stream' in chunk:
-          for line in chunk['stream'].splitlines():
-            if len(line):
-              print(line.strip('\r\n'))
+      for chunk in self.docker.build(fileobj=temp, rm=True, pull=False, decode=True, tag='bbtest_artifacts-scratch'):
+        if not 'stream' in chunk:
+          continue
+        for line in chunk['stream'].splitlines():
+          l = line.strip(os.linesep)
+          if not len(l):
+            continue
+          print(l)
 
       scratch = self.docker.create_container('bbtest_artifacts-scratch', '/bin/true')
 
@@ -79,21 +83,20 @@ class UnitHelper(object):
         raise Exception(scratch['Warnings'])
 
       tar_name = tempfile.NamedTemporaryFile(delete=True)
-
-      tar_stream, stat = self.docker.get_archive(scratch['Id'], '/tmp/packages/lake.deb')
       with open(tar_name.name, 'wb') as destination:
+        tar_stream, stat = self.docker.get_archive(scratch['Id'], target)
         for chunk in tar_stream:
           destination.write(chunk)
 
       archive = tarfile.TarFile(tar_name.name)
-      archive.extract('lake.deb', '/tmp/packages')
+      archive.extract(os.path.basename(target), os.path.dirname(target))
 
-      (code, result, error) = execute([
-        'dpkg', '-c', '/tmp/packages/lake.deb'
-      ])
-
+      (code, result, error) = execute(['dpkg', '-c', target])
       if code != 0:
         raise RuntimeError('code: {}, stdout: [{}], stderr: [{}]'.format(code, result, error))
+      else:
+        with open('/tmp/reports/blackbox-tests/meta/debian.lake.txt', 'w') as fd:
+          fd.write(result)
 
       self.docker.remove_container(scratch['Id'])
     finally:
@@ -108,26 +111,23 @@ class UnitHelper(object):
 
     os.makedirs("/etc/lake/conf.d", exist_ok=True)
     with open('/etc/lake/conf.d/init.conf', 'w') as fd:
-      for k, v in sorted(options.items()):
-        fd.write('LAKE_{}={}\n'.format(k, v))
+      fd.write(str(os.linesep).join("LAKE_{!s}={!s}".format(k, v) for (k, v) in options.items()))
 
   def cleanup(self):
-    (code, result, error) = execute([
-      'systemctl', 'list-units', '--no-legend'
-    ])
-    result = [item.split(' ')[0].strip() for item in result.split('\n')]
-    result = [item for item in result if ("lake" in item)]
-
-    for unit in result:
-      (code, result, error) = execute([
-        'journalctl', '-o', 'cat', '-u', unit, '--no-pager'
-      ])
+    for unit in self.__get_systemd_units():
+      (code, result, error) = execute(['journalctl', '-o', 'cat', '-u', unit, '--no-pager'])
       if code != 0 or not result:
         continue
-      with open('/tmp/reports/blackbox-tests/logs/{}.log'.format(unit), 'w') as f:
-        f.write(result)
+      with open('/tmp/reports/blackbox-tests/logs/{}.log'.format(unit), 'w') as fd:
+        fd.write(result)
 
   def teardown(self):
-    for unit in ['lake-relay', 'lake']:
+    for unit in self.__get_systemd_units():
       execute(['systemctl', 'stop', unit])
     self.cleanup()
+
+  def __get_systemd_units(self):
+    (code, result, error) = execute(['systemctl', 'list-units', '--no-legend'])
+    result = [item.split(' ')[0].strip() for item in result.split(os.linesep)]
+    result = [item for item in result if "lake" in item]
+    return result
--- bbtest/steps/orchestration_steps.py
+++ bbtest/steps/orchestration_steps.py
@@ -4,21 +4,18 @@
 from behave import *
 from helpers.shell import execute
 import os
+from helpers.eventually import eventually
 
 
 @given('package {package} is {operation}')
 def step_impl(context, package, operation):
   if operation == 'installed':
-    (code, result, error) = execute([
-      "apt-get", "install", "-f", "-qq", "-o=Dpkg::Use-Pty=0", "-o=Dpkg::Options::=--force-confdef", "-o=Dpkg::Options::=--force-confnew", "/tmp/packages/{}.deb".format(package)
-    ])
-    assert code == 0, str(result) + ' ' + str(error)
+    (code, result, error) = execute(["apt-get", "install", "-f", "-qq", "-o=Dpkg::Use-Pty=0", "-o=Dpkg::Options::=--force-confold", "/tmp/packages/{}.deb".format(package)])
+    assert code == 0, "unable to install with code {} and {} {}".format(code, result, error)
     assert os.path.isfile('/etc/lake/conf.d/init.conf') is True
   elif operation == 'uninstalled':
-    (code, result, error) = execute([
-      "apt-get", "-y", "remove", package
-    ])
-    assert code == 0, str(result) + ' ' + str(error)
+    (code, result, error) = execute(["apt-get", "-y", "remove", package])
+    assert code == 0, "unable to uninstall with code {} and {} {}".format(code, result, error)
     assert os.path.isfile('/etc/lake/conf.d/init.conf') is False
   else:
     assert False
@@ -27,65 +24,76 @@ def step_impl(context, package, operation):
 @given('systemctl contains following active units')
 @then('systemctl contains following active units')
 def step_impl(context):
-  (code, result, error) = execute([
-    "systemctl", "list-units", "--no-legend"
-  ])
-  assert code == 0, str(result) + ' ' + str(error)
+  (code, result, error) = execute(["systemctl", "list-units", "--no-legend"])
+  assert code == 0
+
   items = []
   for row in context.table:
     items.append(row['name'] + '.' + row['type'])
-  result = [item.split(' ')[0].strip() for item in result.split('\n')]
+
+  result = [item.split(' ')[0].strip() for item in result.split(os.linesep)]
   result = [item for item in result if item in items]
+
   assert len(result) > 0, 'units not found'
 
 
 @given('systemctl does not contain following active units')
 @then('systemctl does not contain following active units')
 def step_impl(context):
-  (code, result, error) = execute([
-    "systemctl", "list-units", "--no-legend"
-  ])
-  assert code == 0, str(result) + ' ' + str(error)
+  (code, result, error) = execute(["systemctl", "list-units", "--no-legend"])
+  assert code == 0
+
   items = []
   for row in context.table:
     items.append(row['name'] + '.' + row['type'])
-  result = [item.split(' ')[0].strip() for item in result.split('\n')]
+
+  result = [item.split(' ')[0].strip() for item in result.split(os.linesep)]
   result = [item for item in result if item in items]
+
   assert len(result) == 0, 'units found'
 
 
+@given('unit "{unit}" is running')
 @then('unit "{unit}" is running')
 def unit_running(context, unit):
-  (code, result, error) = execute([
-    "systemctl", "show", "-p", "SubState", unit
-  ])
-  assert code == 0, str(result) + ' ' + str(error)
-  assert 'SubState=running' in result
+  @eventually(10)
+  def wait_for_unit_state_change():
+    (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
+    assert code == 0, code
+    assert 'SubState=running' in result, result
+
+  wait_for_unit_state_change()
 
 
+@given('unit "{unit}" is not running')
 @then('unit "{unit}" is not running')
 def unit_not_running(context, unit):
-  (code, result, error) = execute([
-    "systemctl", "show", "-p", "SubState", unit
-  ])
+  (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
   assert code == 0, str(result) + ' ' + str(error)
-  assert 'SubState=dead' in result
+  assert 'SubState=dead' in result, str(result) + ' ' + str(error)
 
 
+@given('{operation} unit "{unit}"')
 @when('{operation} unit "{unit}"')
 def operation_unit(context, operation, unit):
-  (code, result, error) = execute([
-    "systemctl", operation, unit
-  ])
+  (code, result, error) = execute(["systemctl", operation, unit])
   assert code == 0, str(result) + ' ' + str(error)
   if operation == 'restart':
     unit_running(context, unit)
 
 
-@given('lake is configured with')
-def unit_is_configured(context):
+@given('{unit} is configured with')
+def unit_is_configured(context, unit):
   params = dict()
   for row in context.table:
     params[row['property']] = row['value']
   context.unit.configure(params)
-  operation_unit(context, 'restart', 'lake-relay.service')
+
+  (code, result, error) = execute([
+    'systemctl', 'list-units', '--no-legend'
+  ])
+  result = [item.split(' ')[0].strip() for item in result.split(os.linesep)]
+  result = [item for item in result if ("{}-".format(unit) in item and ".service" in item)]
+
+  for unit in result:
+    operation_unit(context, 'restart', unit)
