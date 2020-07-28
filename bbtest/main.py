e75197745088bcc368187530cc0fbe22e741f366
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-07-28 21:11:45 +0200

    wip

--- bbtest/main.py
+++ bbtest/main.py
@@ -5,6 +5,7 @@ import os
 import sys
 import json
 import behave2cucumber
+from helpers.shell import execute
 
 
 if __name__ == "__main__":
@@ -33,18 +34,27 @@ if __name__ == "__main__":
     'reports/blackbox-tests/meta',
     'reports/blackbox-tests/data',
     'reports/blackbox-tests/behave',
-    'reports/blackbox-tests/cucumber'
+    'reports/blackbox-tests/cucumber',
+    'reports/blackbox-tests/junit'
   ]:
     os.system('mkdir -p {}'.format(path))
     os.system('rm -rf {}/*'.format(path))
 
   from behave import __main__ as behave_executable
+
   exit_code = behave_executable.main(args=' '.join(args))
 
   with open('reports/blackbox-tests/behave/results.json', 'r') as fd_behave:
+    cucumber_data = None
     with open('reports/blackbox-tests/cucumber/results.json', 'w') as fd_cucumber:
       behave_data = json.loads(fd_behave.read())
       cucumber_data = json.dumps(behave2cucumber.convert(behave_data))
       fd_cucumber.write(cucumber_data)
+
+  execute([
+    'json_to_junit',
+    'reports/blackbox-tests/cucumber/results.json',
+    'reports/blackbox-tests/junit/results.xml'
+  ])
 
   sys.exit(exit_code)
