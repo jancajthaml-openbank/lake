3a0cb9360514e103c836f4f136a9a9dc9717d2af
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-11-09 21:25:57 +0100

    increase UT coverage

--- services/lake/boot/init.go
+++ services/lake/boot/init.go
@@ -55,8 +55,8 @@ func Initialize() Program {
 	)
 
 	var daemons = make([]utils.Daemon, 0)
-	daemons = append(daemons, metricsDaemon)
-	daemons = append(daemons, relayDaemon)
+	daemons = append(daemons, &metricsDaemon)
+	daemons = append(daemons, &relayDaemon)
 
 	return Program{
 		interrupt: make(chan os.Signal, 1),
