d4b1e748ecd7cdf9da8c4b6dbab1c491206916f4
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-11-12 08:24:42 +0100

    increase ut coverage

--- services/lake/metrics/metrics_test.go
+++ services/lake/metrics/metrics_test.go
@@ -6,32 +6,41 @@ import (
 	"time"
 )
 
+func TestMetrics(t *testing.T) {
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	t.Log("does not panic on invalid root")
+	{
+		NewMetrics(ctx, false, "/dev/null", time.Hour)
+	}
+
+	t.Log("does not panic on nil reference function calls")
+	{
+		var entity *Metrics
+		entity.MessageIngress()
+		entity.MessageEgress()
+		entity.MemoryAllocatedSnapshot()
+	}
+}
+
 func TestMetricsIngress(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	entity := NewMetrics(ctx, false, "/tmp/ingress", time.Hour)
 
-	t.Log("noop on nil reference")
-	{
-		var entity *Metrics
+	if uint64(0) != entity.messageIngress {
+		t.Errorf("extected messageIngress 0 actual %d", entity.messageIngress)
+	}
+
+	for i := 1; i <= 1000; i++ {
 		entity.MessageIngress()
 	}
 
-	t.Log("properly updates ingress messages")
-	{
-		entity := NewMetrics(ctx, false, "/tmp/ingress", time.Hour)
-
-		if uint64(0) != entity.messageIngress {
-			t.Errorf("extected messageIngress 0 actual %d", entity.messageIngress)
-		}
-
-		for i := 1; i <= 1000; i++ {
-			entity.MessageIngress()
-		}
-
-		if uint64(1000) != entity.messageIngress {
-			t.Errorf("extected messageIngress 10000 actual %d", entity.messageIngress)
-		}
+	if uint64(1000) != entity.messageIngress {
+		t.Errorf("extected messageIngress 10000 actual %d", entity.messageIngress)
 	}
 }
 
@@ -39,27 +48,18 @@ func TestMetricsEgress(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	t.Log("noop on nil reference")
-	{
-		var entity *Metrics
+	entity := NewMetrics(ctx, false, "/tmp/egress", time.Hour)
+
+	if uint64(0) != entity.messageEgress {
+		t.Errorf("extected messageEgress 0 actual %d", entity.messageEgress)
+	}
+
+	for i := 1; i <= 1000; i++ {
 		entity.MessageEgress()
 	}
 
-	t.Log("properly updates egress messages")
-	{
-		entity := NewMetrics(ctx, false, "/tmp/egress", time.Hour)
-
-		if uint64(0) != entity.messageEgress {
-			t.Errorf("extected messageEgress 0 actual %d", entity.messageEgress)
-		}
-
-		for i := 1; i <= 1000; i++ {
-			entity.MessageEgress()
-		}
-
-		if uint64(1000) != entity.messageEgress {
-			t.Errorf("extected messageEgress 10000 actual %d", entity.messageEgress)
-		}
+	if uint64(1000) != entity.messageEgress {
+		t.Errorf("extected messageEgress 10000 actual %d", entity.messageEgress)
 	}
 }
 
@@ -67,24 +67,15 @@ func TestMetricsMemory(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	t.Log("noop on nil reference")
-	{
-		var entity *Metrics
-		entity.MemoryAllocatedSnapshot()
+	entity := NewMetrics(ctx, false, "/tmp/memory", time.Hour)
+
+	if uint64(0) != entity.memoryAllocated {
+		t.Errorf("extected memoryAllocated 0 actual %d", entity.memoryAllocated)
 	}
 
-	t.Log("properly updates ingress messages")
-	{
-		entity := NewMetrics(ctx, false, "/tmp/memory", time.Hour)
+	entity.MemoryAllocatedSnapshot()
 
-		if uint64(0) != entity.memoryAllocated {
-			t.Errorf("extected memoryAllocated 0 actual %d", entity.memoryAllocated)
-		}
-
-		entity.MemoryAllocatedSnapshot()
-
-		if uint64(0) == entity.memoryAllocated {
-			t.Errorf("extected memoryAllocated to be non zero after snapshot")
-		}
+	if uint64(0) == entity.memoryAllocated {
+		t.Errorf("extected memoryAllocated to be non zero after snapshot")
 	}
 }
