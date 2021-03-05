e21df2d90959d1eda76f227019526d605320326c
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-03-05 18:42:56 +0100

    wip

--- services/lake/main/src/main.rs
+++ services/lake/main/src/main.rs
@@ -17,26 +17,29 @@ fn main() {
     stopping();
 }
 
+/// tries to notify host os that service is ready
 fn ready() {
     if let Err(e) = notify("READY=1") {
         println!("unable to notify host os about READY with {}", e);
     }
 }
 
+/// tries to notify host os that service is stopping
 fn stopping() {
     if let Err(e) = notify("STOPPING=1") {
         println!("unable to notify host os about STOPPING with {}", e)
     }
 }
 
-fn notify(state: &str) -> io::Result<()> {
+/// sends msg to `NOTIFY_SOCKET` via udp
+fn notify(msg: &str) -> io::Result<()> {
     let socket_path = match env::var_os("NOTIFY_SOCKET") {
         Some(path) => path,
         None => return Ok(()),
     };
     let sock = UnixDatagram::unbound()?;
-    let len = sock.send_to(state.as_bytes(), socket_path)?;
-    if len == state.len() {
+    let len = sock.send_to(msg.as_bytes(), socket_path)?;
+    if len == msg.len() {
         Ok(())
     } else {
         Err(io::Error::new(io::ErrorKind::WriteZero, "incomplete write"))
