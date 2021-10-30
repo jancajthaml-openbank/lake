90a99005bcbdcc857456adf862dcbe7667eafda8
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-10-30 10:02:43 +0200

    bbtests human readable subprocess signals

--- bbtest/steps/orchestration_steps.py
+++ bbtest/steps/orchestration_steps.py
@@ -11,13 +11,13 @@ from helpers.eventually import eventually
 def step_impl(context, package, operation):
   if operation == 'installed':
     (code, result, error) = execute(["apt-get", "install", "-f", "-qq", "-o=Dpkg::Use-Pty=0", "-o=Dpkg::Options::=--force-confold", context.unit.binary])
-    assert code == 0, "unable to install with code {} and {} {}".format(code, result, error)
+    assert code == 'OK', "unable to install with code {} and {} {}".format(code, result, error)
     assert os.path.isfile('/etc/lake/conf.d/init.conf') is True
   elif operation == 'uninstalled':
     (code, result, error) = execute(["apt-get", "-y", "remove", package])
-    assert code == 0, "unable to uninstall with code {} and {} {}".format(code, result, error)
+    assert code == 'OK', "unable to uninstall with code {} and {} {}".format(code, result, error)
     (code, result, error) = execute(["apt-get", "-y", "purge", package])
-    assert code == 0, "unable to purge with code {} and {} {}".format(code, result, error)
+    assert code == 'OK', "unable to purge with code {} and {} {}".format(code, result, error)
     assert os.path.isfile('/etc/lake/conf.d/init.conf') is False
   else:
     assert False, 'unknown operation {}'.format(operation)
@@ -27,7 +27,7 @@ def step_impl(context, package, operation):
 @then('systemctl contains following active units')
 def step_impl(context):
   (code, result, error) = execute(["systemctl", "list-units", "--all", "--no-legend", "--state=active", ])
-  assert code == 0, str(result) + ' ' + str(error)
+  assert code == 'OK', str(result) + ' ' + str(error)
   items = []
   for row in context.table:
     items.append(row['name'] + '.' + row['type'])
@@ -40,7 +40,7 @@ def step_impl(context):
 @then('systemctl does not contain following active units')
 def step_impl(context):
   (code, result, error) = execute(["systemctl", "list-units", "--all", "--no-legend", "--state=active"])
-  assert code == 0, str(result) + ' ' + str(error)
+  assert code == 'OK', str(result) + ' ' + str(error)
   items = []
   for row in context.table:
     items.append(row['name'] + '.' + row['type'])
@@ -55,7 +55,7 @@ def unit_running(context, unit):
   @eventually(10)
   def wait_for_unit_state_change():
     (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
-    assert code == 0, str(result) + ' ' + str(error)
+    assert code == 'OK', str(result) + ' ' + str(error)
     assert 'SubState=running' in result, result
 
   wait_for_unit_state_change()
@@ -67,7 +67,7 @@ def unit_not_running(context, unit):
   @eventually(10)
   def wait_for_unit_state_change():
     (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
-    assert code == 0, str(result) + ' ' + str(error)
+    assert code == 'OK', str(result) + ' ' + str(error)
     assert 'SubState=running' not in result, str(result) + ' ' + str(error)
 
   wait_for_unit_state_change()
@@ -77,7 +77,7 @@ def unit_not_running(context, unit):
 @when('{operation} unit "{unit}"')
 def operation_unit(context, operation, unit):
   (code, result, error) = execute(["systemctl", operation, unit])
-  assert code == 0, str(result) + ' ' + str(error)
+  assert code == 'OK', str(result) + ' ' + str(error)
 
 
 @given('{unit} is configured with')
