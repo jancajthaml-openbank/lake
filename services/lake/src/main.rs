3f03fdebf36767a0d9efc2bc1fade76a213cdf77
Skoky <skokys@gmail.com>
2021-11-07 16:20:07 +0100

    updated rust sources

--- services/lake/src/main.rs
+++ services/lake/src/main.rs
@@ -4,7 +4,8 @@ mod config;
 use std::process::exit;
 use std::sync::Arc;
 use zmq::Message;
-use std::io;
+use std::{env, io};
+use std::os::unix::net::UnixDatagram;
 use tokio::io::Error;
 use crate::config::Configuration;
 use crate::metrics::Metrics;
@@ -14,9 +15,12 @@ use log::{info, warn, debug, error, LevelFilter};
 // #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(),Error> {
 
+	println!("STARTING ABC");
+	ready();
 	let metrics = Metrics::new(&Configuration::load());
 	let ctx = zmq::Context::new();
 
+	println!("STARTING B");
 	let mut socket_pull = ctx.socket(zmq::PULL).unwrap();
 	socket_pull.set_conflate(false);
 	socket_pull.set_immediate(true);
@@ -24,6 +28,7 @@ async fn main() -> Result<(),Error> {
 	socket_pull.set_rcvhwm(0);
 	socket_pull.connect("tcp://127.0.0.1:5562").unwrap();
 
+	println!("STARTING C");
 	let mut socket_pub = ctx.socket(zmq::PUB).unwrap();
 	socket_pub.connect("tcp://127.0.0.1:5561").unwrap();
 	socket_pub.set_conflate(false);
@@ -31,11 +36,14 @@ async fn main() -> Result<(),Error> {
 	socket_pub.set_linger(0);
 	socket_pub.set_rcvhwm(0);
 
+	println!("STARTING D");
+
 	// setup_logging()
 	debug!("Starting");
 	info!("Starting");
 	warn!("Starting");
 	error!("Starting");
+
 
 	let mut msg: Message = Message::new();
 	loop {
@@ -49,6 +57,7 @@ async fn main() -> Result<(),Error> {
 			}
 			Err(e) => {
 				eprintln!("{}", e);
+				stopping();
 				exit(0);
 			}
 		}
@@ -81,3 +90,33 @@ async fn main() -> Result<(),Error> {
 //
 // 	Ok(())
 // }
+
+
+/// tries to notify host os that service is ready
+fn ready() {
+	if let Err(e) = notify("READY=1") {
+		println!("unable to notify host os about READY with {}", e);
+	}
+}
+
+/// tries to notify host os that service is stopping
+fn stopping() {
+	if let Err(e) = notify("STOPPING=1") {
+		println!("unable to notify host os about STOPPING with {}", e)
+	}
+}
+
+/// sends msg to `NOTIFY_SOCKET` via udp
+fn notify(msg: &str) -> io::Result<()> {
+	let socket_path = match env::var_os("NOTIFY_SOCKET") {
+		Some(path) => path,
+		None => return Ok(()),
+	};
+	let sock = UnixDatagram::unbound()?;
+	let len = sock.send_to(msg.as_bytes(), socket_path)?;
+	if len == msg.len() {
+		Ok(())
+	} else {
+		Err(io::Error::new(io::ErrorKind::WriteZero, "incomplete write"))
+	}
+}
