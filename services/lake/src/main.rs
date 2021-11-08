1451b30e4b5628398b75e384cbd10ea9918a38ca
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-11-08 20:10:59 +0100

    fix

--- services/lake/src/main.rs
+++ services/lake/src/main.rs
@@ -1,8 +1,8 @@
-use std::{env, io};
 use std::os::unix::net::UnixDatagram;
 use std::process::exit;
-use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
+use std::sync::Arc;
+use std::{env, io};
 
 use tokio::io::Error;
 use tokio::sync::mpsc;
@@ -17,69 +17,69 @@ mod metrics;
 //#[tokio::main(flavor = "multi_thread")]
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Error> {
-	let config = Configuration::load();
+    let config = Configuration::load();
 
-	// STUB for setup logging and bbtests
-	println!("Log level set to {}", config.log_level);
+    // STUB for setup logging and bbtests
+    println!("Log level set to {}", config.log_level);
 
-	ready();
+    ready();
 
-	let mut socket_pull = zeromq::PullSocket::new();
-	socket_pull
-		.bind(&format!("tcp://127.0.0.1:{}", config.pull_port))
-		.await
-		.expect("Failed to bind PULL socket");
+    let mut socket_pull = zeromq::PullSocket::new();
+    socket_pull
+        .bind(&format!("tcp://127.0.0.1:{}", config.pull_port))
+        .await
+        .expect("Failed to bind PULL socket");
 
-	let mut socket_pub = zeromq::PubSocket::new();
-	socket_pub
-		.bind(&format!("tcp://127.0.0.1:{}", config.pub_port))
-		.await
-		.expect("Failed to bind PUB socket");
+    let mut socket_pub = zeromq::PubSocket::new();
+    socket_pub
+        .bind(&format!("tcp://127.0.0.1:{}", config.pub_port))
+        .await
+        .expect("Failed to bind PUB socket");
 
-	// FIXME better with tokio not native thread
-	let stub_term = Arc::new(AtomicBool::new(false));
+    // FIXME better with tokio not native thread
+    let stub_term = Arc::new(AtomicBool::new(false));
 
-	let metrics = Metrics::new(&config);
-	metrics.start(stub_term.clone());
+    let metrics = Metrics::new(&config);
+    metrics.start(stub_term.clone());
 
-	let (sub_results_sender, mut sub_results) = mpsc::channel::<ZmqMessage>(1_000_000);
+    let (sub_results_sender, mut sub_results) = mpsc::channel::<ZmqMessage>(1_000_000);
 
-	tokio::spawn(async move {
-		loop {
-			match sub_results.recv().await {
-				Some(m) => {
-					socket_pub.send(m);
-				}
-				None => {
-					eprintln!("Error processing queue {}", e);
-					stopping();
-					exit(1)
-				}
-			}
-		}
-	});
+    tokio::spawn(async move {
+        loop {
+            match sub_results.recv().await {
+                Some(m) => {
+                    socket_pub.send(m);
+                }
+                None => {
+                    eprintln!("Error processing queue");
+                    stopping();
+                    exit(1)
+                }
+            }
+        }
+    });
 
-	loop {
-		match socket_pull.recv().await {
-			Ok(m) => {
-				metrics.message_ingress();
-				match sub_results_sender.send(m).await {
-					Ok(_) => {
-						metrics.message_egress();  // TODO this would be better to have in receiving queue
-					}
-					Err(e) => {
-						eprintln!("Error sending to queue {}", e);
-						stopping();
-						exit(1);
-					}
-				}
-			}
-			Err(e) => {
-				// TODO eprintln!("ZMQ error {}", e);
-				stopping()
-			}
-		}
-	}
+    loop {
+        match socket_pull.recv().await {
+            Ok(m) => {
+                metrics.message_ingress();
+                match sub_results_sender.send(m).await {
+                    Ok(_) => {
+                        metrics.message_egress(); // TODO this would be better to have in receiving queue
+                    }
+                    Err(e) => {
+                        eprintln!("Error sending to queue");
+                        stopping();
+                        exit(1);
+                    }
+                }
+            }
+            Err(_) => {
+                eprintln!("ZMQ error");
+                stopping()
+            }
+        }
+    }
 }
 
 // fn setup_logging(&self) -> Result<(), LifecycleError> {
@@ -109,29 +109,29 @@ async fn main() -> Result<(), Error> {
 
 /// tries to notify host os that service is ready
 fn ready() {
-	if let Err(e) = notify("READY=1") {
-		println!("unable to notify host os about READY with {}", e);
-	}
+    if let Err(e) = notify("READY=1") {
+        println!("unable to notify host os about READY with {}", e);
+    }
 }
 
 /// tries to notify host os that service is stopping
 fn stopping() {
-	if let Err(e) = notify("STOPPING=1") {
-		println!("unable to notify host os about STOPPING with {}", e)
-	}
+    if let Err(e) = notify("STOPPING=1") {
+        println!("unable to notify host os about STOPPING with {}", e)
+    }
 }
 
 /// sends msg to `NOTIFY_SOCKET` via udp
 fn notify(msg: &str) -> io::Result<()> {
-	let socket_path = match env::var_os("NOTIFY_SOCKET") {
-		Some(path) => path,
-		None => return Ok(()),
-	};
-	let sock = UnixDatagram::unbound()?;
-	let len = sock.send_to(msg.as_bytes(), socket_path)?;
-	if len == msg.len() {
-		Ok(())
-	} else {
-		Err(io::Error::new(io::ErrorKind::WriteZero, "incomplete write"))
-	}
+    let socket_path = match env::var_os("NOTIFY_SOCKET") {
+        Some(path) => path,
+        None => return Ok(()),
+    };
+    let sock = UnixDatagram::unbound()?;
+    let len = sock.send_to(msg.as_bytes(), socket_path)?;
+    if len == msg.len() {
+        Ok(())
+    } else {
+        Err(io::Error::new(io::ErrorKind::WriteZero, "incomplete write"))
+    }
 }
