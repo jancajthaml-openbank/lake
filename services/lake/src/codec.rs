9a3e70c81429b0f3cfad8c26c221558da7735fdc
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-11-10 19:03:11 +0100

    separate codec into its own crate

---
+++ services/lake/src/codec.rs
@@ -0,0 +1,72 @@
+use tokio::io::AsyncReadExt;
+use tokio::net::tcp::ReadHalf;
+
+pub async fn read_frame<'a>(reader: &mut ReadHalf<'a>) -> Result<(Vec<u8>, bool, bool), String> {
+    let frame_type = match reader.read_u8().await {
+        Ok(v) => v,
+        Err(_) => return Err("Failed to read first byte of Frame".to_owned()),
+    };
+
+    let mut frame_buffer = match frame_type {
+        0x00 | 0x01 | 0x04 => match reader.read_u8().await {
+            Ok(size) => vec![0 as u8; size as usize],
+            Err(_) => return Err("Failed to read size of Frame".to_owned()),
+        },
+        0x02 | 0x03 | 0x06 => {
+            match reader.read_u64().await {
+                Ok(size) => {
+                    if size >= usize::MAX as u64 {
+                        return Err("Failed to read size of Frame, size is larger than this system can handle".to_owned());
+                    } else {
+                        vec![0 as u8; size as usize]
+                    }
+                }
+                Err(_) => return Err("Failed to read size of Frame".to_owned()),
+            }
+        }
+        _ => return Err("Invalid frame header size".to_owned()),
+    };
+
+    let is_last = match frame_type {
+        0x00 | 0x02 | 0x04 | 0x06 => true,
+        _ => false,
+    };
+
+    let is_cmd = match frame_type {
+        0x04 | 0x06 => true,
+        _ => false,
+    };
+
+    let n = match reader.read_exact(&mut frame_buffer).await {
+        Ok(v) => v,
+        Err(_) => return Err("Error Reading TCP Packet".to_owned()),
+    };
+
+    if n != frame_buffer.len() {
+        return Err("Short read".to_owned());
+    }
+
+    Ok((frame_buffer, is_last, is_cmd))
+}
+
+pub async fn read_message<'a>(reader: &mut ReadHalf<'a>) -> Result<Vec<u8>, String> {
+    let mut frame = match read_frame(reader).await {
+        Ok((_, _, true)) => return Err("Command red instead of Message".to_owned()),
+        Ok((a, true, false)) => return Ok(a),
+        Ok((a, false, false)) => a,
+        Err(_) => return Err("Failed to read Frame".to_owned()),
+    };
+    loop {
+        let (next_frame, is_last) = match read_frame(reader).await {
+            Ok((_, _, true)) => {
+                return Err("Next Frame of Message changed its type to Command".to_owned())
+            }
+            Ok((a, b, _)) => (a, b),
+            Err(_) => return Err("Failed to read Frame".to_owned()),
+        };
+        frame.extend_from_slice(&next_frame);
+        if is_last {
+            return Ok(frame);
+        }
+    }
+}
--- services/lake/src/pull.rs
+++ services/lake/src/pull.rs
@@ -7,17 +7,18 @@ use tokio::sync::mpsc;
 use tokio::sync::mpsc::Receiver;
 use tokio::task::JoinHandle;
 
+use crate::codec::{read_frame, read_message};
+
 pub struct PullRoutine {
     receiver: Receiver<Vec<u8>>,
     handler: JoinHandle<u8>,
 }
 
-/*
 impl Drop for PullRoutine {
     fn drop(&mut self) {
-        drop(self.handler)
+        drop(&self.handler)
     }
-}*/
+}
 
 impl PullRoutine {
     #[must_use]
@@ -201,75 +202,5 @@ async fn handshake<'a>(
     match writer.write_all(&handshake_reply).await {
         Ok(_) => Ok(()),
         Err(_) => Err("Failed to Send Handshake to Client".to_owned()),
-    }
-}
-
-async fn read_frame<'a>(reader: &mut ReadHalf<'a>) -> Result<(Vec<u8>, bool, bool), String> {
-    let frame_type = match reader.read_u8().await {
-        Ok(v) => v,
-        Err(_) => return Err("Failed to read first byte of Frame".to_owned()),
-    };
-
-    let mut frame_buffer = match frame_type {
-        0x00 | 0x01 | 0x04 => match reader.read_u8().await {
-            Ok(size) => vec![0 as u8; size as usize],
-            Err(_) => return Err("Failed to read size of Frame".to_owned()),
-        },
-        0x02 | 0x03 | 0x06 => {
-            match reader.read_u64().await {
-                Ok(size) => {
-                    if size >= usize::MAX as u64 {
-                        return Err("Failed to read size of Frame, size is larger than this system can handle".to_owned());
-                    } else {
-                        vec![0 as u8; size as usize]
-                    }
-                }
-                Err(_) => return Err("Failed to read size of Frame".to_owned()),
-            }
-        }
-        _ => return Err("Invalid frame header size".to_owned()),
-    };
-
-    let is_last = match frame_type {
-        0x00 | 0x02 | 0x04 | 0x06 => true,
-        _ => false,
-    };
-
-    let is_cmd = match frame_type {
-        0x04 | 0x06 => true,
-        _ => false,
-    };
-
-    let n = match reader.read_exact(&mut frame_buffer).await {
-        Ok(v) => v,
-        Err(_) => return Err("Error Reading TCP Packet".to_owned()),
-    };
-
-    if n != frame_buffer.len() {
-        return Err("Short read".to_owned());
-    }
-
-    Ok((frame_buffer, is_last, is_cmd))
-}
-
-async fn read_message<'a>(reader: &mut ReadHalf<'a>) -> Result<Vec<u8>, String> {
-    let mut frame = match read_frame(reader).await {
-        Ok((_, _, true)) => return Err("Command red instead of Message".to_owned()),
-        Ok((a, true, false)) => return Ok(a),
-        Ok((a, false, false)) => a,
-        Err(_) => return Err("Failed to read Frame".to_owned()),
-    };
-    loop {
-        let (next_frame, is_last) = match read_frame(reader).await {
-            Ok((_, _, true)) => {
-                return Err("Next Frame of Message changed its type to Command".to_owned())
-            }
-            Ok((a, b, _)) => (a, b),
-            Err(_) => return Err("Failed to read Frame".to_owned()),
-        };
-        frame.extend_from_slice(&next_frame);
-        if is_last {
-            return Ok(frame);
-        }
     }
 }
