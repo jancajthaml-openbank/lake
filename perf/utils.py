45a50d6f7056b79105d053e55f2b06126192e67d
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-01-04 21:12:42 +0100

    perf tests tty detection fix

--- perf/utils.py
+++ perf/utils.py
@@ -27,7 +27,7 @@ this.__progress_running = False
 
 termios.tcsetattr(fd, termios.TCSANOW, new)
 
-__TTY = sys.stdout.isatty() and (int(os.environ.get('CI', 0)) == 0)
+__TTY = sys.stdout.isatty() and str(os.environ.get('CI', 'false')) == 'true'
 
 if not __TTY:
   print()
